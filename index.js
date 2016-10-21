417b43de74f082f78ba539ae617f4e6c97854150
Martin Stovicek <martin.stovicek+oss@gmail.com>
2016-10-21 14:58:16 +0200

    Make encrypting and decrypting progressive/async with streams

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 var CryptoJS = require('crypto-js');
 var reduxPersist = require('redux-persist');
+var Stream = require('readable-stream');
 var stringify = require('json-stringify-safe');
 var createTransform = reduxPersist.createTransform;
+
+class ProgressiveCryptor {
+  constructor(state, secretKey) {
+    var salt = CryptoJS.lib.WordArray.random(8);
+    var cipher = CryptoJS.kdf.OpenSSL.execute(secretKey, 8, 4, salt);
+
+    this.state = state;
+    this.key = CryptoJS.enc.Utf8.parse(secretKey);
+
+    this.cryptorParams = {
+      iv: cipher.iv,
+    };
+  }
+
+  start() {
+    var stream = new Stream;
+    var processedState = '';
+
+    stream
+    .on('data', (data) => {
+      processedState += this.processor.process(data.toString());
+    })
+    .on('end', () => {
+      processedState += this.processor.finalize();
+      return processedState;
+    });
+
+    stream.push(this.state);
+    stream.push(null);
+  }
+
+  encrypt() {
+    this.processor = CryptoJS.algo.AES.createEncryptor(this.key, this.cryptorParams);
+    this.start();
+  }
+
+  decrypt() {
+    this.processor = CryptoJS.algo.AES.createDecryptor(this.key, this.cryptorParams);
+    this.start();
+  }
+}
 
 function createEncryptor(secretKey) {
   return function (state, key) {
@@ -9,7 +51,7 @@ function createEncryptor(secretKey) {
       state = stringify(state);
     }
 
-    return CryptoJS.AES.encrypt(state, secretKey).toString();
+    return new ProgressiveCryptor(state, secretKey).encrypt();
   }
 }
 
@@ -24,7 +66,7 @@ function createDecryptor(secretKey) {
     }
 
     try {
-      var bytes = CryptoJS.AES.decrypt(state, secretKey);
+      var bytes = new ProgressiveCryptor(state, secretKey).decrypt();
       var newState = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
       return newState;
