b4e44530ceda61fd9328e9e8c0226154a53a6844
Martin Stovicek <martin.stovicek+oss@gmail.com>
2016-10-23 15:14:35 +0200

    Make progressive ciphering optional

---
+++ src/ProgressiveCryptor.js
@@ -0,0 +1,49 @@
+var CryptoJS = require('crypto-js');
+var Stream = require('readable-stream');
+
+export default class ProgressiveCryptor {
+  constructor(state, secretKey) {
+    var salt = CryptoJS.lib.WordArray.random(8);
+    var cipher = CryptoJS.kdf.OpenSSL.execute(secretKey, 8, 4, salt);
+
+    this.state = state;
+    this.key = CryptoJS.enc.Utf8.parse(secretKey);
+
+    this.cryptorParams = {
+      iv: cipher.iv,
+    };
+  }
+
+  start(marker) {
+    new Promise((resolve, reject) => {
+      var stream = new Stream;
+      var processedState = '';
+
+      try {
+        stream
+        .on('data', (data) => {
+          processedState += this.processor.process(data.toString());
+        })
+        .on('end', () => {
+          processedState += this.processor.finalize();
+          resolve(processedState);
+        });
+
+        stream.push(this.state);
+        stream.push(null);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  encrypt() {
+    this.processor = CryptoJS.algo.AES.createEncryptor(this.key, this.cryptorParams);
+    this.start('encrypt');
+  }
+
+  decrypt() {
+    this.processor = CryptoJS.algo.AES.createDecryptor(this.key, this.cryptorParams);
+    this.start('decrypt');
+  }
+}
