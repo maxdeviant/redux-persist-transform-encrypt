5ad60a0a0e5cdcd3df792964af2d5685d4e22a2a
noymer <bxhshjp2000@yahoo.co.jp>
2017-11-30 07:56:59 +0900

    append test

--- src/__tests__/sync.test.js
+++ src/__tests__/sync.test.js
@@ -2,49 +2,78 @@ import createEncryptor from '../sync';
 global.console.error = jest.fn();
 
 describe('sync', () => {
-  it('can encrypt incoming state', () => {
-    const encryptTransform = createEncryptor({
-      secretKey: 'redux-is-awesome'
-    });
-    const key = 'testState';
-    const state = {
+  const testStates = new Map([
+    ['object', {
       foo: 'bar'
-    };
-    const newState = encryptTransform.in(state, key);
-    expect(typeof newState).toBe('string');
-    expect(newState).not.toEqual(state);
-  });
+    }],
+    ['string', 'just string'],
+    ['number', 123],
+    ['boolean', true],
+    ['null', null],
+  ]);
 
-  it('can decrypt outgoing state', () => {
-    const encryptTransform = createEncryptor({
-      secretKey: 'redux-is-awesome'
-    });
-    const key = 'testState';
-    const state = {
-      foo: 'bar'
-    };
-    const encryptedState = encryptTransform.in(state, key);
-    const newState = encryptTransform.out(encryptedState, key);
-    expect(newState).toEqual(state);
-  });
+  for (let [description, state] of testStates) {
+    describe('state: ' + description, () => {
+      it('can encrypt incoming state', () => {
+        const encryptTransform = createEncryptor({
+          secretKey: 'redux-is-awesome'
+        });
+        const key = 'testState';
+        const newState = encryptTransform.in(state, key);
+        expect(typeof newState).toBe('string');
+        expect(newState).not.toEqual(state);
+      });
 
-  it('should show a nice error message when an incorrect key is provided', () => {
-    const initialEncryptTransform = createEncryptor({
-      secretKey: 'super-secret'
+      it('can decrypt outgoing state', () => {
+        const encryptTransform = createEncryptor({
+          secretKey: 'redux-is-awesome'
+        });
+        const key = 'testState';
+        const encryptedState = encryptTransform.in(state, key);
+        const newState = encryptTransform.out(encryptedState, key);
+        expect(newState).toEqual(state);
+      });
+
+      it('should show a nice error message when an incorrect key is provided', () => {
+        const initialEncryptTransform = createEncryptor({
+          secretKey: 'super-secret'
+        });
+        const key = 'testState';
+        const encryptedState = initialEncryptTransform.in(state, key);
+        const encryptTransform = createEncryptor({
+          secretKey: 'different-secret'
+        });
+        const newState = encryptTransform.out(encryptedState, key);
+        expect(global.console.error).toHaveBeenCalledWith(
+          new Error(
+            'Could not decrypt state. Please verify that you are using the correct secret key.'
+          )
+        );
+      });
     });
-    const key = 'testState';
-    const state = {
-      foo: 'bar'
-    };
-    const encryptedState = initialEncryptTransform.in(state, key);
-    const encryptTransform = createEncryptor({
-      secretKey: 'different-secret'
+  }
+
+  let state = undefined;
+  describe('state: undefined', () => {
+    it('can encrypt incoming state', () => {
+      const encryptTransform = createEncryptor({
+        secretKey: 'redux-is-awesome'
+      });
+      const key = 'testState';
+      const newState = encryptTransform.in(state, key);
+      expect(typeof newState).toBe('string');
+      expect(newState).not.toEqual(state);
     });
-    const newState = encryptTransform.out(encryptedState, key);
-    expect(global.console.error).toHaveBeenCalledWith(
-      new Error(
-        'Could not decrypt state. Please verify that you are using the correct secret key.'
-      )
-    );
+
+    it('undefined will be decrypted to null', () => {
+      const encryptTransform = createEncryptor({
+        secretKey: 'redux-is-awesome'
+      });
+      const key = 'testState';
+      const encryptedState = encryptTransform.in(state, key);
+      const newState = encryptTransform.out(encryptedState, key);
+      expect(newState).toEqual(null);
+      expect(newState).not.toEqual(state);
+    });
   });
 });
