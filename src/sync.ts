8f7a962890bb8b0f166b86e5fffb4a974963257c
Danila, Stavrov <dan.stavrov@gmail.com>
2021-12-22 13:31:23 +0300

    Added generic return type with State

--- src/sync.ts
+++ src/sync.ts
@@ -1,7 +1,7 @@
 import * as Aes from 'crypto-js/aes';
 import * as CryptoJsCore from 'crypto-js/core';
 import stringify from 'json-stringify-safe';
-import { createTransform } from 'redux-persist';
+import { createTransform, Transform } from 'redux-persist';
 
 export interface EncryptTransformConfig {
   secretKey: string;
@@ -11,7 +11,7 @@ export interface EncryptTransformConfig {
 const makeError = (message: string) =>
   new Error(`redux-persist-transform-encrypt: ${message}`);
 
-export const encryptTransform = (config: EncryptTransformConfig) => {
+export function encryptTransform<State = any>(config: EncryptTransformConfig): Transform<any, string, State, State> {
   if (typeof config === 'undefined') {
     throw makeError('No configuration provided.');
   }
@@ -22,36 +22,36 @@ export const encryptTransform = (config: EncryptTransformConfig) => {
   }
 
   const onError =
-    typeof config.onError === 'function' ? config.onError : console.warn;
+      typeof config.onError === 'function' ? config.onError : console.warn;
 
   return createTransform(
-    (inboundState, _key) =>
-      Aes.encrypt(stringify(inboundState), secretKey).toString(),
-    (outboundState, _key) => {
-      if (typeof outboundState !== 'string') {
-        return onError(makeError('Expected outbound state to be a string.'));
-      }
-
-      try {
-        const decryptedString = Aes.decrypt(outboundState, secretKey).toString(
-          CryptoJsCore.enc.Utf8
-        );
-        if (!decryptedString) {
-          throw new Error('Decrypted string is empty.');
+      (inboundState, _key) =>
+          Aes.encrypt(stringify(inboundState), secretKey).toString(),
+      (outboundState, _key) => {
+        if (typeof outboundState !== 'string') {
+          return onError(makeError('Expected outbound state to be a string.'));
         }
 
         try {
-          return JSON.parse(decryptedString);
+          const decryptedString = Aes.decrypt(outboundState, secretKey).toString(
+              CryptoJsCore.enc.Utf8
+          );
+          if (!decryptedString) {
+            throw new Error('Decrypted string is empty.');
+          }
+
+          try {
+            return JSON.parse(decryptedString);
+          } catch {
+            return onError(makeError('Failed to parse state as JSON.'));
+          }
         } catch {
-          return onError(makeError('Failed to parse state as JSON.'));
+          return onError(
+              makeError(
+                  'Could not decrypt state. Please verify that you are using the correct secret key.'
+              )
+          );
         }
-      } catch {
-        return onError(
-          makeError(
-            'Could not decrypt state. Please verify that you are using the correct secret key.'
-          )
-        );
       }
-    }
   );
-};
+}
